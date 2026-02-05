0ba436dbf2f245bfd53d1a158cdbf0b6e743ce38
mami-hedi <hedimemi@gmail.com>
2026-02-05 13:46:24 +0100

    modification db

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,14 +1,22 @@
 import mysql from "mysql2/promise";
-
 import dotenv from "dotenv";
+
 dotenv.config();
 
 export const db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "DarMamieDida",
+  // On utilise les variables d'environnement ou les valeurs locales par défaut
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "",
+  database: process.env.DB_NAME || "DarMamieDida",
+  port: process.env.DB_PORT || 3306,
+  
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
+  
+  // Crucial pour Aiven : configuration SSL
+  ssl: {
+    rejectUnauthorized: false // Permet la connexion sécurisée sans certificat CA local
+  }
 });
